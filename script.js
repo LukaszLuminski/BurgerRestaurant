9c683fa6cc1321118c95b0918184e9fb7e7d5fe1
Lukasz Luminski <lukk87@gmail.com>
2020-02-18 00:20:53 +0000

    styling first commit

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-$('.second-button').on('click', function () {
+$('.second-button').on('click', function() {
 
-    $('.animated-icon2').toggleClass('open');
-  });
+  $('.animated-icon2').toggleClass('open');
+});
 
-  $(window).on('scroll', function() {
 
-  });
+$(function() {
+  $("#mdb-lightbox-ui").load("mdb/mdb-addons/mdb-lightbox-ui.html");
+});
 
-  // object-fit polyfill run
-  // objectFitImages();
+$(".copyright").text(new Date().getFullYear());
 
-  // MDB Lightbox Init
-$(function () {
-$("#mdb-lightbox-ui").load("mdb/mdb-addons/mdb-lightbox-ui.html");
+
+$(document).ready(function(){
+    w_w = $(window).width();
+    if(w_w <= 992){
+        $("#navbarSupportedContent").addClass('dropright');
+    }
+
+    $(window).resize(function(){
+    w_w = $(window).width();
+    if(w_w >= 993){
+        $("#navbarSupportedContent").removeClass('dropright');
+    } else {
+      $("#navbarSupportedContent").addClass('dropright');
+    }
+    });
 });
