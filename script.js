0a1d6abc4932e2bda222ce20c2ebd71c8aa4acc6
Lukasz Luminski <lukk87@gmail.com>
2020-02-18 14:06:43 +0000

    working on header

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 $('.second-button').on('click', function() {
 
   $('.animated-icon2').toggleClass('open');
+  wW = $(window).width();
+  if(wW <= 992) {
+    if ($('.animated-icon2').hasClass('open')) {
+      $('.navbar').attr('style', 'background: #f3e5d8; box-shadow: 1px 4px 15px 5px rgba(0, 0, 0, 0.35);');
+      $('.animated-icon2 span').attr('style', 'background-color: black;');
+      $('.nav-link:not(.active)').attr('style', 'color: black;');
+  } else {
+    $('.navbar').attr('style', 'background: transparent; box-shadow: unset;');
+    $('.animated-icon2 span').attr('style', 'background-color: white;');
+    $('.nav-link:not(.active)').attr('style', 'color: white;');
+    }
+  }
+
+});
+
+$(window).scroll(function(){
+if(wW <= 992) {
+ if ($('.navbar').hasClass('top-nav-collapse')) {
+   $('.navbar').attr('style', 'background: #f3e5d8; box-shadow: 1px 4px 15px 5px rgba(0, 0, 0, 0.35);');
+   $('.animated-icon2 span').attr('style', 'background-color: black;');
+   $('.nav-link:not(.active)').attr('style', 'color: black;');
+ } else if ((!$('.navbar').hasClass('top-nav-collapse'))&&(!$('.animated-icon2').hasClass('open'))){
+   $('.navbar').attr('style', 'background: transparent; box-shadow: unset;');
+   $('.animated-icon2 span').attr('style', 'background-color: white;');
+   $('.nav-link:not(.active)').attr('style', 'color: white;');
+ }
+}
+
 });
 
 
@@ -12,14 +40,14 @@ $(".copyright").text(new Date().getFullYear());
 
 
 $(document).ready(function(){
-    w_w = $(window).width();
-    if(w_w <= 992){
+    wW = $(window).width();
+    if(wW <= 992){
         $("#navbarSupportedContent").addClass('dropright');
-    }
+      }
 
     $(window).resize(function(){
-    w_w = $(window).width();
-    if(w_w >= 993){
+    wW = $(window).width();
+    if(wW >= 993){
         $("#navbarSupportedContent").removeClass('dropright');
     } else {
       $("#navbarSupportedContent").addClass('dropright');
